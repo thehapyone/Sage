bfee93b39db0760baf25fe323a6b52fb77c91f90
Ayo Ayibiowu <charlesayibiowu@hotmail.com>
2024-08-25 23:08:48 +0200

    feat: adds support to forcefully update sources

--- sage/data_loader.py
+++ sage/data_loader.py
@@ -13,10 +13,17 @@ async def notify_chat_service():
     await SENTINEL_PATH.write_text("updated")
 
 
-async def update_sources():
-    """A task for updating the sources on schedule"""
+async def update_sources(refresh: bool = False):
+    """
+    A task for updating the sources on schedule
+
+
+    Args:
+        refresh (bool, optional): A flag for reindexing the data sources.
+    """
     logger.info("Triggering sources update")
-    await Source().run()
+    await Source().run(refresh)
+    logger.info("Source update complete")
     # Send an update signal to the chat service
     await notify_chat_service()
 
@@ -31,11 +38,11 @@ async def main():
         return
 
     # Run the update_sources function immediately before starting the scheduler
-    await update_sources()
+    await update_sources(refresh=False)
 
     interval = CronTrigger.from_crontab(scheduler_time)
     scheduler = AsyncIOScheduler(gconfig={"logger": logger})
-    scheduler.add_job(update_sources, trigger=interval)
+    scheduler.add_job(update_sources, trigger=interval, args=[True])
     scheduler.start()
     logger.info("Data Loader Scheduler started, now running forever.")
     while True:
